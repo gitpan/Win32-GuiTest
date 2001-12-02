3576ffc6c1aefb805e06c9cfb6dedfd0fc9c7772
Ernesto Guisado <erngui@cpan.org>
2001-12-02 00:19:46 +0000

    Import of ERNGUI/Win32-GuiTest-1.3 from CPAN.
    gitpan-cpan-distribution: Win32-GuiTest
    gitpan-cpan-version:      1.3
    gitpan-cpan-path:         ERNGUI/Win32-GuiTest-1.3.zip
    gitpan-cpan-author:       ERNGUI
    gitpan-cpan-maturity:     released

--- makedist.bat
+++
@@ -1,8 +0,0 @@
-nmake
-call pod2text <GuiTest.pm >README
-tar cvf Win32-GuiTest.tar blib
-gzip --best Win32-GuiTest.tar
-nmake ppd
-perl -npi.bak -e "s|<CODEBASE HREF=\"\" />|<CODEBASE HREF=\"Win32-GuiTest.tar.gz\" />|;" Win32-GuiTest.ppd
-nmake zipdist
-
